7f8d46068a0780ac6aa90759300ae501335263ce
Diego Orellana <diego.orellanab@pgn.gob.gt>
2018-09-26 13:42:32 -0600

    I coded to functions CRUD

--- web/src/config/Alimento/ls_alimento.js
+++ web/src/config/Alimento/ls_alimento.js
@@ -5,9 +5,14 @@ function getAlimentos()
     return JSON.parse(localStorage.getItem('Alimentos'));
 }
 
+function setAlimentos(alimentos)
+{
+    localStorage.setItem('Alimentos',JSON.stringify(alimentos));
+}
+
 export function startup()
 {
-    localStorage.setItem('Alimentos',JSON.stringify(Alimentos)); 
+    setAlimentos(Alimentos);
 }
 
 export function getPagineo(start, length)
@@ -25,12 +30,78 @@ export function getPagineo(start, length)
     };
 }
 
+export function insertAlimento(alimento)
+{
+    let alimentos = getAlimentos();
+    alimentos.push(alimento);
+    setAlimentos(alimentos);
+    return {
+            "ok":true,
+            alimento
+        };
+}
+
+export function deleteAlimento(id)
+{
+    let alimentos = getAlimentos();
+    let alimento = alimentos.find(x => x._id === id);
+    if(alimento === undefined)
+    {
+        return {
+            "ok":false,
+            err: {message: "No se ha encontrado el alimento"}
+        };
+    }
+    else
+    {
+        alimentos = alimentos.filter(x => x._id !== id);
+        setAlimentos(alimentos);
+                return {
+            "ok":true,
+            alimento
+        };
+    }
+}
+
 export function getAlimento(id)
 {
     let alimentos = getAlimentos();
     let alimento = alimentos.find(x => x._id === id);
+    if(alimento === undefined)
+    {
         return {
-        "ok":true,
-        alimento
-    };
+            "ok":false,
+            err: {message: "No se ha encontrado el alimento"}
+        };
+    }
+    else
+    {
+        return {
+            "ok":true,
+            alimento
+        };
+    }
 }
+
+export function updateAlimento(alimento)
+{
+    let alimentos = getAlimentos();
+    let indexAlimento = alimentos.findIndex(x => x._id === alimento._id);
+    if(indexAlimento === -1)
+    {
+        return {
+            "ok":false,
+            err: {message: "No se ha encontrado el alimento"}
+        };
+    }
+    else
+    {
+        alimentos[indexAlimento] = alimento;
+        setAlimentos(alimentos);
+        return {
+            "ok":true,
+            alimento
+        };
+    }
+}
+
