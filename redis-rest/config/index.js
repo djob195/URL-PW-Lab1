babecaba06fcb1804ba3fce32d20c77a81dffe24
diego orellana <djorellanab@outlook.com>
2018-11-19 00:36:34 -0600

    remove ports

--- redis-rest/config/index.js
+++ redis-rest/config/index.js
@@ -1,7 +1,7 @@
 // ==============================
 // Port
 // ==============================
-process.env.PORT =  process.env.PORT || 8080;
+process.env.PORT =  process.env.PORT || 8081;
 
 process.env.REST_API = process.env.REST_API || "http://rest-web:3000/";
 process.env.REDIS_PORT = process.env.REDIS_PORT || '6379';
