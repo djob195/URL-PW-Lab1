f75f32ed277e2367b97284ab7e095a4b27f5e4f9
djob195 <djorellanab@outlook.com>
2018-11-19 10:09:09 -0600

    update

--- web/src/global.js
+++ web/src/global.js
@@ -4,5 +4,5 @@ global.dataByRow = 4;
 global.colLength =  parseInt(12/global.dataByRow,10);
 
 // Configuración de API
-global.restApi = "http://redis-client-service/";
+global.restApi = "http://35.225.143.122/";
 global.fakeFetch = "http://localhost/helloWorld.html";
